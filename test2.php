a71ea1cbb70cbb1bc81c6f4107eda8fe91727aa9
Chris Kreidler <ckreidler@gmail.com>
2016-08-07 23:10:37 -0700

    Add stuff

---
+++ test2.php
@@ -0,0 +1,373 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JSano Fan Server</title>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-66064409-1', 'auto');
+	ga('send', 'pageview');
+</script>
+<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/superhero/bootstrap.min.css" rel="stylesheet" integrity="sha256-o0IkLyCCWGBI+ryg6bL44/f8s4cb7+5bncR4LvU57a8= sha512-jptu6vg45XTY9uPX3vD5nHN4vASCN2hHl+fhmgkdd/px/bFHKMXmDXhkNmTiCpKqH6sliEPFakl2KZNav2Zo1Q==" crossorigin="anonymous">
+<link href="template.css" rel="stylesheet" type="text/css">
+<link href="stylesheet.css" rel="stylesheet" type="text/css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div id="major_background"></div>
+<div class="container" id="main">
+	<br>
+    <div class="row jmenu">
+    	<div class="col-sm-2 col-sm-offset-1 active"><a href="#">Home</a></div>
+        <div class="col-sm-2"><a href="members/">Members</a></div>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-2"><a href="map/">Overviewer</a></div>
+        <div class="col-sm-2"><a href="stats/">Stats</a></div>
+        
+    </div>
+	<br>
+    <div id="background">
+    
+    
+    	<!-- Carousel -->
+        <div id="banners" class="carousel slide" data-ride="carousel" data-pause="hover">
+
+            <!--<ol class="carousel-indicators">
+                <li data-target="#banners" data-slide-to="0" class="active"></li>
+                <li data-target="#banners" data-slide-to="1"></li>
+                <li data-target="#banners" data-slide-to="2"></li>
+                <li data-target="#banners" data-slide-to="3"></li>
+                <li data-target="#banners" data-slide-to="4"></li>
+                <li data-target="#banners" data-slide-to="5"></li>
+            </ol>-->
+            
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="images/banner0.jpg" alt="Town Hall at Night">
+                    <div class="carousel-caption">
+                    	<p>Town Hall at Night</p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner1.jpg" alt="Muckluck's Castle">
+                    <div class="carousel-caption">
+                    	<p>Muckluck's Castle</p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner2.jpg" alt="Town Hall During the Day">
+                    <div class="carousel-caption">
+                    	<p>Town Hall During the Day</p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner3.jpg" alt="JSFS' Nether Hub">
+                    <div class="carousel-caption">
+                    	<p>JSFS' Nether Hub</p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner4.jpg" alt="Matthamew's Palace">
+                    <div class="carousel-caption">
+                    	<p>Matthamew's Palace</p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner5.jpg" alt="Ace's Hockey Stadium">
+                    <div class="carousel-caption">
+                    	<p>Ace's Hockey Stadium<br>
+                        <span class="photo_cred">Photo by Kyle</span></p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner6.jpg" alt="Flash's Kwik-E-Mart">
+                    <div class="carousel-caption">
+                    	<p>Flash's Kwik-E-Mart<br>
+                        <span class="photo_cred">Photo by SuperFlash</span></p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner7.jpg" alt="JSano Land">
+                    <div class="carousel-caption">
+                    	<p>JSano Land<br>
+                        <span class="photo_cred">Photo by StreedCredCookie</span></p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner8.jpg" alt="JSano Kart">
+                    <div class="carousel-caption">
+                    	<p>JSano Kart<br>
+                        <span class="photo_cred">Photo by StreedCredCookie</span></p>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <img src="images/banner9.jpg" alt="A Town on the JFS">
+                    <div class="carousel-caption">
+                    	<p>A Town on the JFS<br>
+                        <span class="photo_cred">Photo by Shawstin</span></p>
+                    </div>
+                </div>
+            </div>
+            
+            <a class="left carousel-control" href="#banners" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#banners" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <!-- End Carousel -->
+        
+        
+        
+        <div class="row">
+        	<div class="col-md-4">
+            	<h2>Status</h2><br>
+                <div class="row">
+                	<div class="col-sm-2 col-sm-offset-4">
+						<?php
+                        /*
+						require __DIR__ . "/src/MinecraftQuery.php";
+                        require __DIR__ . "/src/MinecraftQueryException.php";
+                        
+                        use xPaw\MinecraftQuery;
+                        use xPaw\MinecraftQueryException;
+                        
+						$online = TRUE;
+						$version = "";
+						$player_amnt = "";
+						
+                        $Query = new MinecraftQuery( );
+                        try {
+							$Query->Connect( 'localhost', 25565 );
+							$status = $Query->GetInfo( );
+							echo "<div id='online' class='status-circle'><span class='glyphicon glyphicon-ok'></span></div>Online";
+							$version = $status["Version"];
+							$player_amnt = $status["Players"] . "/" . $status["MaxPlayers"];
+                        }
+                        catch( MinecraftQueryException $e ) {
+							echo "<div id='offline' class='status-circle'><span class='glyphicon glyphicon-remove'></span></div>Offline";
+							$online = FALSE;
+                        }
+						*/
+                        ?>
+                    </div>
+                    <div class="col-sm-4" style="text-align:left;">
+                    <?php
+					/*
+					if($online){
+                    	echo "<strong>Version:</strong> <span class='status-info'>" . $version . "</span><br><strong>Players:</strong> <span class='status-info'>" . $player_amnt . "</span>";
+					}else{
+						echo "Message us on skype to get the server back up";
+					}
+					*/
+					?>
+                    
+                    
+                    </div>
+                </div>
+                
+                <br />
+                <br />
+                
+                <h2>Players Online</h2>
+				<?php
+                /*
+                try {
+                    $Query->Connect( 'localhost', 25565 );
+                    $status = $Query->GetInfo( );
+                    $players = $Query->GetPlayers( );
+                    if ( $status["Players"] == "0" ) {
+                        echo "<div id='nobody' data-toggle='tooltip' data-placement='left' title='Nobody Online'><span class='glyphicon glyphicon-remove'></span></div>";
+                    }
+                    else {
+                        foreach ( $players as $player ) {
+                            echo "<img class='online-face' src='https://minotar.net/helm/" . $player . "/50.png' data-toggle='tooltip' data-placement='left' title='" . $player . "'>";
+                        }
+                    }
+                }
+                catch( MinecraftQueryException $e ) {
+                    echo "<div id='nobody' data-toggle='tooltip' data-placement='left' title='Nobody Online'><span class='glyphicon glyphicon-remove'></span></div>";
+                }
+                */
+                ?>
+                
+                
+                
+            </div>
+            <div class="col-md-3">
+            	<h2>TeamSpeak</h2>
+                
+                <?php
+				require_once("req/ts3/TeamSpeak3.php");
+				
+				$ts3_VirtualServer = TeamSpeak3::factory("serverquery://admin:FZ2rCXYm@jsanofanserver.com:10011/?server_port=9987");
+				
+				echo $ts3_VirtualServer->getViewer(new TeamSpeak3_Viewer_Html("images/viewericons/", "images/countryflags/", "data:image"));
+				?>
+            </div>
+            <div class="col-md-5" id="reddit">
+            	<h2><a href="https://www.reddit.com/r/jsanofanserver/" target="_blank">Recent Posts</a></h2>
+                
+                <div id="chevron_up" class="invisible"><span class="glyphicon glyphicon-chevron-up"></span></div>
+                <div id="postContainer">
+                	<div id="posts_replace_new" class="posts"></div>
+                	<div id="posts" class="posts"></div>
+                    <div id="posts_replace_old" class="posts"></div>
+                </div>
+                <div id="chevron_down"><span class="glyphicon glyphicon-chevron-down"></span></div>
+            </div>
+        </div>
+    </div>
+    <div id="ip">IP :: jsanofanserver.com<span id="full" class="glyphicon glyphicon-info-sign pull-right" data-toggle="tooltip" data-placement="right" title="Website by Dakota"></span></div>
+</div>
+<div id="logo"><a href="#"><img src="req/logo.jpg"></a></div>
+<script>
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+<?php
+$raw_json = "https://www.reddit.com/r/jsanofanserver/new.json?limit=30";
+$posts = json_decode(file_get_contents($raw_json), TRUE);
+
+$j_array = "var posts = [";
+foreach($posts["data"]["children"] as $post){
+	$username = $post["data"]["author"];
+	$comments = $post["data"]["num_comments"];
+	$nsfw = $post["data"]["over_18"];
+	$url = $post["data"]["url"];
+	$title = $post["data"]["title"];
+	$perma = $post["data"]["permalink"];
+	$created = $post["data"]["created"];
+	$thumbnail = str_replace("http://", "https://", $post["data"]["thumbnail"]);
+	$score = $post["data"]["score"];
+	$time = $post["data"]["created_utc"];
+	
+	$current_time = time();
+	$time_since = $current_time - $time;
+	
+	if($time_since >= 31556926){ // Year
+		$number = round($time_since/31556926);
+		$human_time = $number . " years";
+		if($number == 1){
+			$human_time = $number . " year";
+		}
+	}else if($time_since >= 2592000){ // Month
+		$number = round($time_since/2592000);
+		$human_time = $number . " months";
+		if($number == 1){
+			$human_time = $number . " month";
+		}
+	}else if($time_since >= 604800){ // Week
+		$number = round($time_since/604800);
+		$human_time = $number . " weeks";
+		if($number == 1){
+			$human_time = $number . " week";
+		}
+	}else if($time_since >= 86400){ // Day
+		$number = round($time_since/86400);
+		$human_time = $number . " days";
+		if($number == 1){
+			$human_time = $number . " day";
+		}
+	}else if($time_since >= 3600){ // Hour
+		$number = round($time_since/3600);
+		$human_time = $number . " hours";
+		if($number == 1){
+			$human_time = $number . " hour";
+		}
+	}else if($time_since >= 60){ // Minute
+		$number = round($time_since/60);
+		$human_time = $number . " minutes";
+		if($number == 1){
+			$human_time = $number . " minute";
+		}
+	}else{
+		$human_time = "Just now!";
+	}
+	
+	$nsfw_label = "";
+	if($nsfw){
+		$nsfw_label = "<span class='label label-danger'>NSFW</span>&bull; ";
+	}
+	
+	$comment_text = $comments . " comments";
+	if($comments == 0){
+		$comment_text = "Comment";
+	}
+	else if($comments = 1){
+		$comment_text = $comments . " comment";
+	}
+	
+	if($thumbnail == "nsfw" || $thumbnail == "self" || $thumbnail == "default"){
+		$thumbnail = "req/logo.jpg";
+	}
+	
+	$j_array .= "\"<div class='post'> <div class='row'> <div class='col-md-1 votes vcenter'>" . $score . "</div><!-- --><div class='col-md-2 rthumbnail vcenter'><img src='" . $thumbnail . "'/></div><!-- --><div class='col-md-9 info vcenter'> <div class='title'><a href='" . $url . "'>" . htmlentities($title) . "</a></div>" . $nsfw_label . "<span class='user'><a href='https://www.reddit.com/u/" . $username . "'>" . $username . "</a></span> &bull; <span class='comments'><a href='https://www.reddit.com" . $perma . "'>" . $comment_text . "</a></span> &bull; <span class='time'>" . $human_time . " ago</span> </div></div></div>\", ";
+}
+
+$j_array = substr($j_array, 0, -2);
+$j_array .= "];";
+
+echo $j_array;
+?>
+
+var page = 1;
+$("#posts").html(posts[0] + posts[1] + posts[2]);
+
+$("#chevron_down").click(function(){
+	if(page == 1){
+		$("#chevron_up").removeClass("invisible");
+	}
+	page++;
+	if(page == 10){
+		$("#chevron_down").addClass("invisible");
+	}
+	
+	$("#posts_replace_old").html(posts[(page*3)-3] + posts[(page*3)-2] + posts[(page*3)-1]);
+	
+	$("#posts").animate({top: '-100%'}, 500);
+	$("#posts_replace_old").animate({top: '0'}, 500, 0, function(){
+		$("#posts").html(posts[(page*3)-3] + posts[(page*3)-2] + posts[(page*3)-1]);
+		$("#posts").css("top", 0);
+		$("#posts_replace_old").css("top", "100%");
+	});
+});
+$("#chevron_up").click(function(){
+	if(page == 10){
+		$("#chevron_down").removeClass("invisible");
+	}
+	page--;
+	if(page == 1){
+		$("#chevron_up").addClass("invisible");
+	}
+	
+	$("#posts_replace_new").html(posts[(page*3)-3] + posts[(page*3)-2] + posts[(page*3)-1]);
+	
+	$("#posts").animate({top: '100%'}, 500);
+	$("#posts_replace_new").animate({top: '0'}, 500, 0, function(){
+		$("#posts").html(posts[(page*3)-3] + posts[(page*3)-2] + posts[(page*3)-1]);
+		$("#posts").css("top", 0);
+		$("#posts_replace_new").css("top", "-100%");
+	});
+});
+</script>
+</body>
+</html>
